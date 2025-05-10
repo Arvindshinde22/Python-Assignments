952683bf9f3538fa3f2434d6d16e55565eac0ff8
Arvindshinde22 <arvindshinde777@gmail.com>
2025-05-10 15:11:16 +0530

    Add files via upload

---
+++ Assignment1_3.py
@@ -0,0 +1,12 @@
+def Add(value1, value2):
+    return value1 + value2
+
+print("Enter first no:")
+no1 = int(input())
+
+print("Enter second no:")
+no2 = int(input())
+
+result = Add(no1, no2)
+
+print("Addition is:", result)
